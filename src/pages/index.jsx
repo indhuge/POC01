9663fe49a2d32878fb34a61a361da30328622861
Gabriela Barbieri <gabriela.barbieri@hotmail.com>
2023-08-16 09:59:19 -0300

    header pronto

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+
+import styles from './Home.module.scss'
+import Header from './components/header'
+export default function Home() {
+  return (
+    <div className={styles.description}>
+      <Header />
+    </div>
+
+  )
+}
