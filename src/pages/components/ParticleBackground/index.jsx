142281b30fc714bad21e9e6a6aaae191fc45e755
EnityBlackHawk <luanf2003@gmail.com>
2023-08-17 15:46:22 -0300

    Background animation added

---
+++ src/pages/components/ParticleBackground/index.jsx
@@ -0,0 +1,28 @@
+import styles from "./ParticleBackground.module.scss";
+
+export default function ParticleBackground() {
+  return (
+    <ul className={styles.circles}>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  );
+}
--- src/pages/components/header/welcome/index.jsx
+++ src/pages/components/header/welcome/index.jsx
@@ -1,19 +1,29 @@
+import ParticleBackground from "../../ParticleBackground";
 import Button from "../botton";
 import Styles from "./welcome.module.scss";
 import Image from "next/image";
 import BannerWelcome from "public/Leads.svg";
 const Welcome = () => {
-    return <div className={Styles.container}>
+  return (
+    <div className={Styles.wrapper}>
+      <div className={Styles.container}>
         <div className={Styles.text}>
-            <h1>Melhor agência de marketing do bairro</h1>
-            <p>Somos uma agencia de performance digital,
-                aceleramos vendas e aquisição de leads para grandes marcas</p>
-            <Button title="Aumentar vendas" kind="secundary" />
+          <h1>Melhor agência de marketing do bairro</h1>
+          <p>
+            Somos uma agencia de performance digital, aceleramos vendas e
+            aquisição de leads para grandes marcas
+          </p>
+          <Button title="Aumentar vendas" kind="secundary" />
         </div>
         <div className={Styles.image}>
-            <Image src={BannerWelcome} alt="Banner Welcome" />
+          <Image src={BannerWelcome} alt="Banner Welcome" />
         </div>
-    </div>;
+      </div>
+      <div className={Styles.particles}>
+        <ParticleBackground/>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
