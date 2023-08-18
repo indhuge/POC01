7b780a103f574fb7818afcbdc7fc7c528dfcd6ad
João Pedro <jcervinobarbosa@gmail.com>
2023-08-18 09:56:11 -0300

    mudando cor de box shadow

--- src/pages/components/select/index.jsx
+++ src/pages/components/select/index.jsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import Style from './select.module.scss';
 
 const Select = ({ placeholder, required, options }) => {
   return (
-    <select required={required} placeholder={placeholder}>
-        {options?.map((option, index) => (
-          <option key={index} value={option.value}>{option.label}</option>
-        ))}
+    <select required={required} className={Style.select}>
+      <option value="" disabled hidden>{placeholder}</option>
+      {options?.map((option, index) => (
+        <option key={index} value={option.value}>{option.label}</option>
+      ))}
     </select>
-  )
-}
+  );
+};
 
 export default Select;
