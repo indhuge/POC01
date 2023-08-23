3c135f21328211d3565adc039210c85e9ab758aa
EnityBlackHawk <luanf2003@gmail.com>
2023-08-23 14:27:58 -0300

    feat: Add the chatbot using Botpress

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,23 @@
-import styles from './Home.module.scss'
-import Header from './components/header'
-import Welcome from './components/header/welcome'
-import Features from './components/features'
-import Contato from './components/contato'
-import Footer from './components/footer'
+import styles from "./Home.module.scss";
+import Header from "./components/header";
+import Welcome from "./components/header/welcome";
+import Features from "./components/features";
+import Contato from "./components/contato";
+import Footer from "./components/footer";
+import { useEffect } from "react";
 
 export default function Home() {
-
-  
+  useEffect(() => {
+    window.botpressWebChat.init({
+      composerPlaceholder: "Chat with IndHelp",
+      botConversationDescription: "Tire suas duvidas",
+      botId: "a65625bb-64c9-4a57-b4db-a7bd2aa1270b",
+      hostUrl: "https://cdn.botpress.cloud/webchat/v0",
+      messagingUrl: "https://messaging.botpress.cloud",
+      clientId: "a65625bb-64c9-4a57-b4db-a7bd2aa1270b",
+      botName: "IndHelp",
+    });
+  });
 
   return (
     <div className={styles.description}>
@@ -16,7 +26,8 @@ export default function Home() {
       <Features />
       <Contato />
       <Footer />
-    </div>
 
-  )
+      <script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
+    </div>
+  );
 }
