246ef348329750e696ccc87c452b0a4c5629910a
João Pedro <101291672+CervinoB@users.noreply.github.com>
2023-08-24 08:48:41 -0300

    PROD-134 Form validation (#1)
    * PROD-134 build: install formik on npm
    
    * PROD-134 build: install npm yup
    
    * PROD-134 feat: send the form with formik

--- src/pages/components/select/index.jsx
+++ src/pages/components/select/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Style from './select.module.scss';
 
-const Select = ({ placeholder, required, options, onChange, name }) => {
+const Select = ({ id, placeholder, required, options, onChange, onBlur, name }) => {
   return (
-    <select required={required} className={Style.select} onChange={onChange} name={name}>
-      <option value="" disabled hidden>{placeholder}</option>
+    <select
+      id={id}
+      required={required}
+      className={Style.select}
+      onChange={onChange}
+      onBlur={onBlur}
+      name={name}
+    >
+      <option value="" disabled hidden>
+        {placeholder}
+      </option>
       {options?.map((option, index) => (
         <option key={index} value={option.value}>{option.label}</option>
       ))}
