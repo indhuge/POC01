ffafe664951b7469e66242a3ac231519e205e60e
João Pedro <jcervinobarbosa@gmail.com>
2023-08-30 16:46:33 -0300

    PROD-275 fix: add alt to images and remove extanded  css
    Just working on the warnings

--- src/pages/components/ParticleBackground/index.jsx
+++ src/pages/components/ParticleBackground/index.jsx
@@ -5,47 +5,47 @@ import Image from "next/image";
 export default function ParticleBackground() {
   return (
     <ul className={styles.circles}>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-      <li><Image src={gear}/></li>
-     
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+      <li><Image alt="Gear" src={gear} /></li>
+
     </ul>
   );
 }
