f1bdf22b83189bd03d48887060952ec8fd3f2fa8
Luan Felipe <luanf2003@gmail.com>
2023-09-05 14:53:03 -0300

    feat: New file structure

---
+++ src/components/Newsletter/index.jsx
@@ -0,0 +1,54 @@
+import Styles from "./Newsletter.module.scss";
+import { useState } from "react";
+import { PrismicRichText } from "@prismicio/react";
+
+async function register(_email) {
+  const res = await fetch("/api/mailChimp", {
+    body: JSON.stringify({
+      email: _email,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  });
+
+  const { error } = await res.json();
+  if (error) console.log(error);
+}
+
+export default function Newsletter({ mainText, inputPlaceholder, buttonText }) {
+  let state = useState({
+    email: "",
+  });
+
+  let onEmailChaged = (e) => {
+    let upEmail = e.target.value;
+    state.email = upEmail;
+  };
+
+  let submit = async (e) => {
+    e.preventDefault();
+    console.log(state.email);
+    register(state.email);
+    state.email = "";
+  };
+
+  return (
+    <div className={Styles.main}>
+      <div className={Styles.content}>
+        <PrismicRichText field={mainText} />
+        <form onSubmit={submit}>
+          <input
+            type="email"
+            name="email"
+            placeholder={inputPlaceholder}
+            value={state.email}
+            onChange={onEmailChaged}
+          />
+          <input type="submit" value={buttonText} />
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,10 @@
-import styles from './Home.module.scss'
-import Header from '../components/header'
-import Welcome from '../components/header/welcome'
-import Features from '../components/features'
-import Contato from '../components/contato'
-import Footer from '../components/footer'
-import Newsletter from '../components/Newsletter'
-import { useEffect } from "react";
+import styles from "./Home.module.scss";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import { useEffect, useState } from "react";
+import { createClient } from "@/prismicio";
+import { SliceZone } from "@prismicio/react";
+import { components } from "@/slices";
 
 function queryContent() {
   const client = createClient();
@@ -13,9 +12,12 @@ function queryContent() {
 }
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState();
+
   useEffect(() => {
     async function loadBotpress() {
-      await window.botpressWebChat.init({
+      await window.botpressWebChat?.init({
         composerPlaceholder: "Chat with IndHelp",
         botConversationDescription: "Tire suas duvidas",
         botId: "a65625bb-64c9-4a57-b4db-a7bd2aa1270b",
@@ -27,6 +29,13 @@ export default function Home() {
     }
     loadBotpress();
   });
+
+  if (!page) {
+    queryContent().then((p) => {
+      setIsLoading(false);
+      setPage(p);
+    });
+  }
 
   if (isLoading) {
     return (
@@ -43,16 +52,25 @@ export default function Home() {
   }
 
   return (
-
     <div className={styles.description}>
-      <Header />
-      <Welcome />
-      <Features />
-      <Newsletter />
-      <Contato />
-      <Footer />
-      <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/43719883.js"></script>
-      <script src="https://cdn.botpress.cloud/webchat/v0/inject.js" async></script>
+      <Header
+        logoUrl={page.data.logo.url}
+        callToActionText={page.data.call_to_action_text}
+        navLinks={page.data.menuitens}
+      />
+      <SliceZone slices={page.data.slices} components={components} />
+      <Footer
+        logoUrl={page.data.logo.url}
+        menuFooterTitle={page.data.menu_footer_title}
+        navLinks={page.data.menuitens}
+        contentTitle={page.data.content_footer_title}
+        contentLinks={page.data.content_menu_options}
+        phoneNumber={page.data.phone_number}
+        email={page.data.email}
+        socialTitle={page.data.social_footer_title}
+        socialLinks={page.data.social_links}
+        copyright={page.data.copyright_text}
+      />
     </div>
   );
 }
