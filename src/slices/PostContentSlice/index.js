748d074f984afa4ac2e13b920c516f4c09daa6d3
Luan Felipe <luanf2003@gmail.com>
2023-09-06 16:17:29 -0300

    PROD-312 feat: Add content in PostContentSlice

--- src/slices/PostContentSlice/index.js
+++ src/slices/PostContentSlice/index.js
@@ -1,16 +1,25 @@
+import { PrismicRichText } from "@prismicio/react";
+
 /**
  * @typedef {import("@prismicio/client").Content.PostTitleSliceSlice} PostTitleSliceSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<PostTitleSliceSlice>} PostTitleSliceProps
  * @param {PostTitleSliceProps}
  */
 const PostTitleSlice = ({ slice }) => {
+  console.log(slice.items);
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for post_title_slice (variation: {slice.variation})
-      Slices
+      <PrismicRichText field={slice.primary.title} />
+      {slice.items.map((e, index) => {
+        return (
+          <div key={index}>
+            <PrismicRichText field={e.content} />
+          </div>
+        );
+      })}
     </section>
   );
 };
