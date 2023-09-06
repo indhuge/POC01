e72169743f89a9719fcd04913df86b5b2b47deec
Luan Felipe <luanf2003@gmail.com>
2023-09-06 16:51:34 -0300

    style: Add styles to BlogPost

--- src/pages/blog/[id].jsx
+++ src/pages/blog/[id].jsx
@@ -2,6 +2,7 @@ import { createClient } from "@/prismicio";
 import { PrismicNextImage } from "@prismicio/next";
 import { SliceZone } from "@prismicio/react";
 import { components } from "@/slices";
+import Styles from "./BlogPost.module.scss";
 
 export async function getStaticProps({ params }) {
   const client = createClient();
@@ -24,12 +25,20 @@ export default function Page({ page }) {
   console.log(page);
   return (
     <>
-      <PrismicNextImage field={page?.data?.main_image} />
-      <h1>{page?.data.post_title}</h1>
-      <p>
-        {page?.data.post_author} <span>{page?.data?.publish_date}</span>
-      </p>
-      <SliceZone slices={page?.data?.slices} components={components} />
-    </> 
+      <div className={Styles.wrapper}>
+        <PrismicNextImage field={page?.data?.main_image} />
+        <div className={Styles.contentContainer}>
+          <div className={Styles.content}>
+            <div className={Styles.title}>
+              <h1>{page?.data.post_title}</h1>
+              <p>
+                Autor: {page?.data.post_author} <span>{page?.data?.publish_date}</span>
+              </p>
+            </div>
+            <SliceZone slices={page?.data?.slices} components={components} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/slices/PostContentSlice/index.js
+++ src/slices/PostContentSlice/index.js
@@ -1,5 +1,5 @@
 import { PrismicRichText } from "@prismicio/react";
-
+import Styles from "./PostContentSlice.module.scss"
 /**
  * @typedef {import("@prismicio/client").Content.PostTitleSliceSlice} PostTitleSliceSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<PostTitleSliceSlice>} PostTitleSliceProps
@@ -9,10 +9,12 @@ const PostTitleSlice = ({ slice }) => {
   console.log(slice.items);
   return (
     <section
+    className={Styles.wrapper}
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
       <PrismicRichText field={slice.primary.title} />
+      
       {slice.items.map((e, index) => {
         return (
           <div key={index}>
