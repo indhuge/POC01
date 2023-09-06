c0e6de2820fae795b03b8c7a5d9ecb2113c111cf
Luan Felipe <luanf2003@gmail.com>
2023-09-06 13:51:51 -0300

    PROD-312 feat: Add content on Blog Post Page

--- src/pages/blog/[id].jsx
+++ src/pages/blog/[id].jsx
@@ -1,4 +1,7 @@
 import { createClient } from "@/prismicio";
+import { PrismicNextImage } from "@prismicio/next";
+import { SliceZone } from "@prismicio/react";
+import { components } from "@/slices";
 
 export async function getStaticProps({ params }) {
   const client = createClient();
@@ -16,7 +19,17 @@ export function getStaticPaths() {
   };
 }
 
-export default function Page(props) {
-  console.log(props);
-  return <h1></h1>;
+export default function Page({ page }) {
+  //TODO: Add a loading state
+  console.log(page);
+  return (
+    <>
+      <PrismicNextImage field={page?.data?.main_image} />
+      <h1>{page?.data.post_title}</h1>
+      <p>
+        {page?.data.post_author} <span>{page?.data?.publish_date}</span>
+      </p>
+      <SliceZone slices={page?.data?.slices} components={components} />
+    </> 
+  );
 }
