fb3f6d316b807468c93255b46c1da5c89dae509f
Luan Felipe <luanf2003@gmail.com>
2023-09-22 14:51:24 -0300

    PROD-343: refactor: Fix error on build

--- src/utils/generate-robots.js
+++ src/utils/generate-robots.js
@@ -6,6 +6,7 @@ Disallow:
 sitemap: ${host}/sitemap.xml
 `;
 function generate_robots() {
+  console.log("Mode: ", process.env.ENV_TYPE);
   const fs = require("fs");
   fs.writeFileSync("public/robots.txt", robots_content);
 }
