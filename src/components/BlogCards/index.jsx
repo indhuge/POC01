c243c454ae4b3d93b1e9951d0adfafb424053da4
Luan Felipe <luanf2003@gmail.com>
2023-09-27 16:22:08 -0300

    PROD-346 style: Add visual effects

--- src/components/BlogCards/index.jsx
+++ src/components/BlogCards/index.jsx
@@ -30,22 +30,67 @@ export default function Component({ category, cards, Key }) {
   const slideLeft = () => {
     var slider = document.getElementById(`blog_card_holder-${Key}`);
     slider.scrollLeft -= window.innerWidth / 2;
+
+    slider.addEventListener("scrollend", (e) => {
+      if (slider.scrollLeft == 0) {
+        var arrowL = document.getElementById(`left_arrwow-${Key}`);
+        arrowL.classList.add(Styles.display_none);
+        console.log(e);
+      }
+    });
   };
 
   const slideRight = () => {
     var slider = document.getElementById(`blog_card_holder-${Key}`);
+    var arrowL = document.getElementById(`left_arrwow-${Key}`);
     slider.scrollLeft += window.innerWidth / 2;
+    arrowL.classList.remove(Styles.display_none);
   };
+
+  const toggleShowAll = () => {
+    var slider = document.getElementById(`blog_card_holder-${Key}`);
+    var arrowL = document.getElementById(`left_arrwow-${Key}`);
+    var arrowR = document.getElementById(`right_arrwow-${Key}`);
+    if (slider.classList.contains(Styles.showAll)) {
+      slider.classList.remove(Styles.showAll);
+      arrowR.classList.remove(Styles.display_none);
+    } else {
+      slider.classList.add(Styles.showAll);
+      arrowL.classList.add(Styles.display_none);
+      arrowR.classList.add(Styles.display_none);
+    }
+  };
+
+  const deactivateArrows = () => {
+    var slider = document.getElementById(`blog_card_holder-${Key}`);
+    if (slider.offsetWidth != slider.scrollWidth) return;
+
+    var arrowL = document.getElementById(`left_arrwow-${Key}`);
+    var arrowR = document.getElementById(`right_arrwow-${Key}`);
+    var showAll = document.getElementById(`show_all-${Key}`);
+
+    arrowL.classList.add(Styles.display_none);
+    arrowR.classList.add(Styles.display_none);
+    showAll.classList.add(Styles.display_none);
+  };
+
+  useEffect(() => deactivateArrows(), []);
 
   return (
     <div className={Styles.wrapper}>
       <div className={Styles.content}>
-        <h1>{category}</h1>
+        <div className={Styles.titleWrapper}>
+          <h2>{category}</h2>
+          <p onClick={toggleShowAll} id={`show_all-${Key}`}>
+            Mostrar tudo
+          </p>
+        </div>
         <div className={Styles.slideWrapper}>
           <MdChevronLeft
-            className={Styles.arrow}
+            className={` ${Styles.arrow} ${Styles.display_none}`}
             onClick={slideLeft}
-            size={40}
+            size={35}
+            id={`left_arrwow-${Key}`}
           />
           <div className={Styles.cardHolder} id={`blog_card_holder-${Key}`}>
             {cards?.map((c, index) => {
@@ -69,10 +114,12 @@ export default function Component({ category, cards, Key }) {
           <MdChevronRight
             className={Styles.arrow}
             onClick={slideRight}
-            size={40}
+            size={35}
+            id={`right_arrwow-${Key}`}
           />
         </div>
       </div>
+      {}
     </div>
   );
 }
