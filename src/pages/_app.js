600dcc20bffa2502ca2d995d1a5320baf7fc215e
Luan Felipe <luanf2003@gmail.com>
2023-09-29 15:09:36 -0300

    feat: Add transition animation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,14 +5,36 @@ import { useRouter } from "next/router";
 import { createClient } from "@/prismicio";
 import StaticContent from "@/utils/StaticContent";
 import "@/components/Calendar/React_calendar.scss";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <div key={router.pathname}>
-      <Component {...pageProps} />
-      <PrismicPreview repositoryName={repositoryName} />
-    </div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={router.pathname}
+        initial="pageInitial"
+        animate="pageAnimate"
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        exit="pageExit"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+          pageExit: {
+            opacity: 0,
+          },
+        }}
+      >
+        <div key={router.pathname}>
+          <Component {...pageProps} />
+          <PrismicPreview repositoryName={repositoryName} />
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
