e4d330f6655c29fc6958448f352daa03ffebf718
EnityBlackHawk <97620478+EnityBlackHawk@users.noreply.github.com>
2023-10-02 13:42:34 -0300

    PROD-174 refactor: Fix Blog Card click navigation error (#51)
    * PROD-174 refactor: Fix Blog Card click navigation error
    
    * PROD-346 feat: Add adaptability when resizing

--- src/components/BlogCards/index.jsx
+++ src/components/BlogCards/index.jsx
@@ -1,7 +1,7 @@
 import { PrismicNextImage } from "@prismicio/next";
 import Styles from "./BlogCards.module.scss";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 function _onClick(id, category, router) {
@@ -17,6 +17,7 @@ const prefetchPath = (id, category, router) => {
 
 export default function Component({ category, cards, Key }) {
   const router = useRouter();
+  const [showAllText, setShowAllText] = useState("Expandir");
 
   useEffect(() => {
     cards?.map(
@@ -54,27 +55,39 @@ export default function Component({ category, cards, Key }) {
     if (slider.classList.contains(Styles.showAll)) {
       slider.classList.remove(Styles.showAll);
       arrowR.classList.remove(Styles.display_none);
+      setShowAllText("Expandir");
     } else {
       slider.classList.add(Styles.showAll);
       arrowL.classList.add(Styles.display_none);
       arrowR.classList.add(Styles.display_none);
+      setShowAllText("Colapsar");
     }
   };
 
   const deactivateArrows = () => {
+    console.log("RESIZE");
     var slider = document.getElementById(`blog_card_holder-${Key}`);
-    if (slider.offsetWidth != slider.scrollWidth) return;
 
     var arrowL = document.getElementById(`left_arrwow-${Key}`);
     var arrowR = document.getElementById(`right_arrwow-${Key}`);
     var showAll = document.getElementById(`show_all-${Key}`);
+
+    if (slider.offsetWidth != slider.scrollWidth) {
+      arrowL.classList.remove(Styles.display_none);
+      arrowR.classList.remove(Styles.display_none);
+      showAll.classList.remove(Styles.display_none);
+      return;
+    }
 
     arrowL.classList.add(Styles.display_none);
     arrowR.classList.add(Styles.display_none);
     showAll.classList.add(Styles.display_none);
   };
 
-  useEffect(() => deactivateArrows(), []);
+  useEffect(() => {
+    deactivateArrows();
+    window.addEventListener("resize", deactivateArrows);
+  }, []);
 
   return (
     <div className={Styles.wrapper}>
@@ -82,7 +95,7 @@ export default function Component({ category, cards, Key }) {
         <div className={Styles.titleWrapper}>
           <h2>{category}</h2>
           <p onClick={toggleShowAll} id={`show_all-${Key}`}>
-            Mostrar tudo
+            {showAllText}
           </p>
         </div>
         <div className={Styles.slideWrapper}>
@@ -98,7 +111,7 @@ export default function Component({ category, cards, Key }) {
                 <div
                   className={Styles.card}
                   key={index}
-                  onClick={() => _onClick(c.uid, router)}
+                  onClick={() => _onClick(c.uid, c.data.categoty.uid, router)}
                 >
                   <div className={Styles.cardText}>
                     <h2>{c?.data.post_title}</h2>
