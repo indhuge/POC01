acbb47fdc5c4f7bfbead0ff5ee46613b55edfeca
Luan Felipe <luanf2003@gmail.com>
2023-10-17 15:27:57 -0300

    PROD-412 feat: Add database for survey's answer

---
+++ src/DataBase/Controller/surveyController.ts
@@ -0,0 +1,20 @@
+export async function addAnswer(
+  name: string,
+  email: string,
+  birth: Date,
+  answer: string
+): Promise<any> {
+  const model = require("../model/survey_answer");
+  const result = await model.create({
+    name: name,
+    email: email,
+    birth: birth.toISOString(),
+    answer: answer,
+  });
+  return result;
+}
+
+export async function getAllAnswers(): Promise<any> {
+  const model = require("../model/survey_answer");
+  return await model.findAll();
+}
