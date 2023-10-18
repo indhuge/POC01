a85947d0227670732194a02ff8e15cbe0c01d460
Luan Felipe <luanf2003@gmail.com>
2023-10-18 16:57:19 -0300

    PROD-449 refactor: Intregates dashboard with Prismic

--- src/pages/survey/dashboard/index.jsx
+++ src/pages/survey/dashboard/index.jsx
@@ -1,18 +1,25 @@
+import { createClient } from "@/prismicio";
 import { host } from "@/utils/SiteProps";
 import { Card, Title, BarChart, Subtitle } from "@tremor/react";
+import QuestionConverter from "@/utils/QuestionsConverter";
 
 export async function getServerSideProps() {
+  const client = createClient();
+  const survey_data = await client.getSingle("survey");
+
   const req = await fetch(`${host}/api/survey`, { method: "GET" });
   const _data = await req.json();
 
   const chartdata = [];
 
+  const qC = new QuestionConverter(survey_data);
+
   chartdata.push(
     Object.keys(_data).map((p) => {
       return {
-        chart_name: p,
+        chart_name: qC.getQuestionByIndex(p),
         data: Object.keys(_data[p]).map((r) => {
-          return { name: r, respostas: _data[p][r] };
+          return { name: qC.getAnswerById(p, r), respostas: _data[p][r] };
         }),
       };
     })
