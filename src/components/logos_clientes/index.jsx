9bc3cdf3b6f35e00a371cba2bd2a5502baea7b0d
Luan Felipe <luanf2003@gmail.com>
2023-10-19 16:40:12 -0300

    PROD-315 refactor: Fix key build error

--- src/components/logos_clientes/index.jsx
+++ src/components/logos_clientes/index.jsx
@@ -21,9 +21,10 @@ const ClientLogosSection = ({ images }) => {
         }
       `}</style>
       <div className="container flex flex-row gap-4 bg-white px-4 py-4 rounded">
-        {images.map((e) => {
+        {images.map((e, i) => {
           return (
             <PrismicNextImage
+              key={i}
               className="object-scale-down h-20"
               field={e.image}
               alt="Logo Cliente 1"
