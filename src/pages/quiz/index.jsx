0279ac80220a91536a0ad6a3a9b0e8317bb48aad
Luan Felipe <luanf2003@gmail.com>
2023-10-24 17:26:58 -0300

    PROD-456 feat: Add Prismic Integration and Actions

--- src/pages/quiz/index.jsx
+++ src/pages/quiz/index.jsx
@@ -3,13 +3,32 @@ import { createClient } from "@/prismicio";
 import { getStaticContent } from "@/utils/StaticContent";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 export async function getStaticProps() {
   const client = createClient();
-  const staticContent = await getStaticContent(client);
+  const quiz_data = await client.getSingle("quiz");
+  const staticContent = getStaticContent(client);
+  const finalObj = {};
+  const temp = quiz_data.data.slices.map((e) => {
+    return {
+      question: e.primary.question,
+      questionid: e.primary.questionid,
+      action: e.primary.action,
+      answers: e.items.map((i) => {
+        return {
+          text: i.anwser,
+          path: i.path,
+        };
+      }),
+    };
+  });
+  temp.forEach((e) => {
+    finalObj[e.questionid] = e;
+  });
 
   return {
-    props: { staticContent },
+    props: { staticContent: await staticContent, quiz_data: finalObj },
   };
 }
 
@@ -40,13 +59,42 @@ const data = {
   },
 };
 
-export default function QuizPage({ staticContent }) {
+function executeAction(_action, router) {
+  if (_action == null || _action == undefined || _action == "none") return;
+
+  const arr_action = _action.slice(1).split("=");
+  const action = arr_action[0];
+  const param = arr_action[1];
+
+  switch (action) {
+    case "alert": {
+      alert(param);
+      break;
+    }
+    case "redirect": {
+      router.push(param);
+      break;
+    }
+    case "log": {
+      console.log(param);
+      break;
+    }
+  }
+}
+
+export default function QuizPage({ staticContent, quiz_data: data }) {
+  const router = useRouter();
+
   const [question, setQuestion] = useState(data.p1.question);
   const [answers, setAnswers] = useState(data.p1.answers);
 
   const onOptionClicked = (path) => {
-    setQuestion(data[path].question);
-    setAnswers(data[path].answers);
+    if (path.charAt(0) == "!") {
+      executeAction(path, router);
+    } else {
+      setQuestion(data[path].question);
+      setAnswers(data[path].answers);
+    }
   };
 
   return (
